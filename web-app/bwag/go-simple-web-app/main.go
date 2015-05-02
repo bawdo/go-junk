f8306453d83f99b22c798f604150bcb02937ac39
Keith Bawden <keith@bawdo.com>
2015-05-02 12:01:23 +1000

    Basic Web App - Markdown Sanbox
    Building Web Apps with Go.

---
+++ web-app/bwag/go-simple-web-app/main.go
@@ -0,0 +1,24 @@
+// Building Web Apps with Go: Basic Web App
+
+package main
+
+import (
+	"github.com/russross/blackfriday"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":"+port, nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
+}
